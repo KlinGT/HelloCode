50eb66e061cf2aa575042a04d330187db7fe64cc
Klin <piter_frst@yahoo.com>
2022-09-29 17:58:16 +0300

    Random

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,8 @@
-﻿int numberA = 32;
-int numderB = 5;
-int result = numberA + numderB;
+﻿// Вариант с генератором случайностей
+
+int numberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
