b784e5050ba16d85a765932098d6524b8292ebb5
Klin <piter_frst@yahoo.com>
2022-10-13 19:10:27 +0300

    Ex014 Sorting By Selection Method

---
+++ Ex014_SortingBySelectionMethod/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.Clear();
+
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+//метод вывода массива на экран.
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//метод упорядочивания массива от мин к макс.
+void SelectoinSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array [minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectoinSort(arr);
+PrintArray(arr);
+
+/*
+
+//метод вывода массива на экран.
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+//метод упорядочивания массива от макс к мин.
+void SelectoinSort(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        int maxPosition = i;
+        for (int j = i; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array [maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectoinSort(arr);
+PrintArray(arr);
+*/
